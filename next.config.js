bf0d44826d2a281f88afbbe94000f16c827f148c
vlo111 <vvcsharp@gmail.com>
2023-05-31 19:43:29 +0400

    set env global config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    REACT_APP_BASE_URL: 'https://dev-apizinhub.analysed.ai/api/'
+  },
+}
 
 module.exports = nextConfig
