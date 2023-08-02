a2b00a72e9dfd66b656a1fe4ced7f7177741addd
vlo111 <vvcsharp@gmail.com>
2023-08-02 19:05:52 +0400

    add table with pagination

--- src/app/admin/requests/@inactive/page.tsx
+++ src/app/admin/requests/@inactive/page.tsx
@@ -1,9 +1,26 @@
 'use client';
-import DataTable from './data-table';
+import DataTable from './components/table';
 import { useGetCompanyList } from '@/api/company/use-get-all-company';
+import Pagination from '@/app/admin/requests/@inactive/components/pagination';
+import { useState } from 'react';
 
 export default () => {
-  const { data, loading } = useGetCompanyList({ limit: 10, offset: 0, statuses: ['PENDING'] });
+  const [currentPage, setCurrentPage] = useState(1);
 
-  return <div className="h-full w-full">{loading === false && <DataTable data={data.result} />}</div>;
+  const { data, loading } = useGetCompanyList({ limit: 10, offset: currentPage + 10, statuses: ['PENDING'] });
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <div className="h-full w-full">
+      {!loading && (
+        <>
+          <DataTable data={data.result} />
+        </>
+      )}
+      <Pagination limit={10} pageSize={currentPage} totalCount={data.count} onChangePage={handlePageChange} />
+    </div>
+  );
 };
