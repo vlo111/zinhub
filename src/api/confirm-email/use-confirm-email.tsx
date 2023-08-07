dea627407236dfeb2ddce76fee95b1a161cab524
vlo111 <vvcsharp@gmail.com>
2023-08-07 12:32:48 +0400

    fix admin request

--- src/api/confirm-email/use-confirm-email.tsx
+++ src/api/confirm-email/use-confirm-email.tsx
@@ -3,14 +3,14 @@ import client from '../client';
 import { useRouter } from 'next/navigation';
 import { PATHS } from '@/helpers/constants';
 
-const url = 'auth/email/confirm';
+const url = 'api/auth/email/confirm';
 
 export const useConfirmEmail = () => {
   const router = useRouter();
 
   const mutation = useMutation<{ data: { token: string } }, unknown, { token: string }>({
     mutationFn: (data: { token: string }) => {
-      return client.put(url, data);
+      return client.post(url, data);
     },
     onSuccess: () => router.push(PATHS.COMPANY_PROFILE),
   });
